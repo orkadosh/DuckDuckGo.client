1aba42aa4c1254c5266352217aafa8c842abc6e6
orkadosh1 <orkadosh@h-Mac-mini-sl-OR.local>
2022-01-04 16:42:46 +0200

    add pagination

--- DuckDuckGo/src/app/app.component.ts
+++ DuckDuckGo/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, ViewChild } from '@angular/core';
 import {SearchService} from './../services/search.service'
 import { FormBuilder } from '@angular/forms';
 import { MatDrawer } from '@angular/material/sidenav';
+import { PageEvent } from '@angular/material/paginator';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,24 +14,39 @@ export class AppComponent {
   searchText:string="";
   pageNumber:number=0;
   defaultIcon:string="i/423b26ad.png";
+
+  //paginator 
+  length = 100;
+  pageSize = 5;
+  pageSizeOptions: number[] = [5, 10, 25, 100];
+  // MatPaginator Output
+  pageEvent: PageEvent;
+
+
   public searchForm = this.formBuilder.group({
     text: '',
   });
 @ViewChild('drawer') drawer : MatDrawer;
 
   constructor(private formBuilder :FormBuilder, public searchService:SearchService ){
-
   }
 submit(){
   debugger
   let seatchText = this.searchForm.controls["text"].value;
- this.searchService.search(seatchText,this.pageNumber);
+ this.searchService.search(seatchText,this.pageSize);
  this.searchService.searchHistory.unshift(seatchText);
+ this.length =this.searchService.links.length;
 }
 onHistoryClicked(index:number){
   debugger
  this.searchForm.controls.text.setValue(this.searchService.searchHistory[index])
  this.submit();
  this.drawer.toggle();
+}
+onPageChange($event:any){
+debugger
+this.searchService.pageNumber= $event.pageIndex;
+this.pageSize= $event.pageSize;
+this.submit();
 }
 }
--- DuckDuckGo/src/services/search.service.ts
+++ DuckDuckGo/src/services/search.service.ts
@@ -10,15 +10,21 @@ export class SearchService {
   public links:any;
   public searchHistory:any[]=[];
  public  isLoding:boolean=false;
+  public totalItems: number=0;
+  public pageNumber=0;
   constructor(private http: HttpClient ) { }
 
 
 
-  public search(text:string, currentPage:number){
+  public search(text:string,pagesize: number){
     this.isLoding=true;
-    this.http.get(`${environment.testServerPath}Search/?text=${text}&pageNumber=${currentPage}`).subscribe(next=>{
+    if(this.searchHistory&& this.searchHistory.length>0 &&  text!= this.searchHistory[0])
+    this.pageNumber=0;
+    this.http.get<any>(`${environment.testServerPath}Search/?text=${text}&pageNumber=${this.pageNumber}&pageSize=${pagesize}`).subscribe(next=>{
       debugger
-this.links=next;
+ let response = next;//["value"];
+ this.links= response["value"]? response["value"]:null;
+ this.totalItems= response["totalItems"]? response["totalItems"]:0;
 this.isLoding=false;
 
     },error=>{
